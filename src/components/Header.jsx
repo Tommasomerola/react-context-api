11fa5b92d39fa3e87d3d5e4c5ae35a054668ad4d
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-26 18:54:16 +0100

    import header, postlist, defaultlayouts

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+// importo dalla libreria installata react-router-dom per la navigazione
+
+import {NavLink } from "react-router-dom";
+
+export default function Header () {
+    return (
+        <header>
+            <div className="nav-bar">
+                {/* NavLink */}
+                <NavLink to="/">HomePage</NavLink>
+                <NavLink to="/chisiamo">Chi siamo</NavLink>
+                <NavLink to="/posts">Lista dei post</NavLink>
+                {/* <NavLink to="/posts/create">add Post</NavLink> */}
+            </div>
+        </header>
+    )
+}
