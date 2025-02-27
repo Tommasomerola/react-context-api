fcd5c0e7e785745ed487ad35eb1c987670f0715a
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-27 20:11:35 +0100

    import PostListUser in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
+// importiamo il coomponente di listato 
+import PostListUser from './../components/PostListUser'
+
 export default function HomePage() {
     return(
         <div>
             <h1>
                 PAGINA "HOMEPAGE"
             </h1>
+            <PostListUser />
         </div>
     )
 }
