a310d07973766e0b0cbe38e831b62c2b38ccb5e1
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-27 20:07:51 +0100

    integrate GlobalContext

--- src/App.jsx
+++ src/App.jsx
@@ -21,27 +21,38 @@ import PostDetailPage from "./pages/PostDetailPage"
 
 function App() {
 
+  // utilizzo dello useState per la gestione dei data (array di oggetti post)
+  const [posts, setPosts] = useState([]);
+
+  // funzione di gestione chiamata all'API
+  function fetchPosts() {
+    axios.get("http://localhost:3000/posts")
+      .then(res => setPosts(res.data))
+      .catch(error => console.log(error))
+  }
+
+  // richiamo la funzione di richiesta dati al caricamento del componente
+  // Solo al primo rendering
+  useEffect(fetchPosts, []);
 
   return (
-    <>
-         <BrowserRouter>
-      <Routes>
-        {/* DefaultLayout viene utilizzato come layout di default per le rotte */}
-        <Route element={<DefaultLayout />}>
-          {/* Definizione delle rotte */}
-          <Route path="/" element={<HomePage />} />
-          <Route path="/chisiamo" element={<ChiSiamo />} />
-          <Route path="/posts">
-            <Route index element={<PostListPage />} />
-            <Route path="create" element={<PostCreatePage />} />
-            <Route path=":id" element={<PostDetailPage />} />
+    <GlobalContext.Provider value={{ posts }} >
+      <BrowserRouter>
+        <Routes>
+          {/* DefaultLayout viene utilizzato come layout di default per le rotte */}
+          <Route element={<DefaultLayout />}>
+            {/* Definizione delle rotte */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/chisiamo" element={<ChiSiamo />} />
+            <Route path="/posts">
+              <Route index element={<PostListPage />} />
+              <Route path="create" element={<PostCreatePage />} />
+              <Route path=":id" element={<PostDetailPage />} />
             </Route>
-        </Route>
-
-
-      </Routes>
-    </BrowserRouter>
-    </>
+            </Route>
+            </Routes>
+      </BrowserRouter>
+    </GlobalContext.Provider>
   )
 }
 
